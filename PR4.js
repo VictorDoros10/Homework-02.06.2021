54b7932459ef507dc6dac5f2f7786e11df383875
Victor Doros <victordoros1@gmail.com>
2021-06-08 07:24:54 +0300

    PR4_modified

--- PR4.js
+++ PR4.js
@@ -1,28 +1,12 @@
     function myFunction(){
-    let element1 = document.createElement("li");
-    let textElement1 = document.createTextNode(1);
-    element1.appendChild(textElement1);
-    document.getElementById("list").appendChild(element1);
+        let element = document.querySelector("ul");
+        for ( let i = 1; i <= 5; i++){
+            let liElement = document.createElement("li");
+            liElement.innerText= 0 + i;
+            element.appendChild(liElement);
+        }
+        console.log(element);
 
-    let element2 = document.createElement("li");
-    let textElement2 = document.createTextNode(2);
-    element2.appendChild(textElement2);
-    document.getElementById("list").appendChild(element2);
-   
-    let element3 = document.createElement("li");
-    let textElement3 = document.createTextNode(3);
-    element3.appendChild(textElement3);
-    document.getElementById("list").appendChild(element3);
-
-    let element4 = document.createElement("li");
-    let textElement4 = document.createTextNode(4);
-    element4.appendChild(textElement4);
-    document.getElementById("list").appendChild(element4);
-
-    let element5 = document.createElement("li");
-    let textElement5 = document.createTextNode(5);
-    element5.appendChild(textElement5);
-    document.getElementById("list").appendChild(element5);
     }
     
 
